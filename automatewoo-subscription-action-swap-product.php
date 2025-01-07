138912f718bdce1befdd356400260c0cdef7cfca
Nick <nick.green@automattic.com>
2025-01-07 13:05:15 -0800

    Update automatewoo-subscription-action-swap-product.php

--- automatewoo-subscription-action-swap-product.php
+++ automatewoo-subscription-action-swap-product.php
@@ -2,8 +2,8 @@
 /*
  * Plugin Name: AutomateWoo Subscription Action - Swap Product
  * Plugin URI:  https://github.com/a8cteam51/automatewoo-subscription-action-swap-product
- * Description: Extends the functionality of AutomateWoo with a custom action which allows you to add a product to an order as a line item.
- * Version:     1.0.0
+ * Description: Extends the functionality of AutomateWoo with a custom action which allows you to swap out products in a subscription.
+ * Version:     1.2.0
  * Author:      WP Special Projects
  * Author URI:  https://wpspecialprojects.wordpress.com/
  * License:     GPL v2 or later
